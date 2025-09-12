8aa48b2c3ae7817656f99a222eeba360f66daf24
Oversoul <oversoul@localhost.localdomain>
2025-09-12 16:42:26 -0400

    recuperar clave funcional

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InicioController;
+use Illuminate\Support\Facades\Auth;
 
+//ruta de inicio
 Route::get('/', [InicioController::class, 'index'])->name('inicio');
 
 Auth::routes();
 
+
+
+//rutas publicas
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+
+// rutas para admin (acceso total)
+
+
+// rutas para usuarios (acceso limitado por permisos)
